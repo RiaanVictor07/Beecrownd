6cba6e6668fc3c2d4bd0679af0ef8259bc1179d2
Rian Santos <santosriaan10@gmail.com>
2026-08-15 11:59:03 -0300

    Desafio Beecrownd

---
+++ src/Intervalo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Intervalo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double valor = sc.nextDouble();
+
+        if (valor < 0 || valor > 100) {
+            System.out.println("Fora de intervalo");
+        } else if (valor <= 25) {
+            System.out.println("Intervalo [0,25]");
+        } else if (valor <= 50) {
+            System.out.println("Intervalo (25,50]");
+        } else if (valor <= 75) {
+            System.out.println("Intervalo (50,75]");
+        } else {
+            System.out.println("Intervalo (75,100]");
+        }
+    }
+}
